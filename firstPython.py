791031195eb6562c7b6db766aab2ace7ae76d435
Koissi <70168615+SaviKoissi@users.noreply.github.com>
2022-08-15 21:53:16 -0400

    Create firstPython.py

---
+++ firstPython.py
@@ -0,0 +1,10 @@
+# Create my first function 
+def comp_mean(x):
+    #Although there is a function 
+    #mean for the purpose of this 
+    #class we are creating a 
+    #function to do the same task. 
+    #x is a vector of numeric 
+    ave = sum(x)/len(x)
+    print("The mean is", ave)
+  
